204df108aa7c76f3ebef9c850f905ab641a7a2e3
malikhijazi <malik.hijazi@gmail.com>
2019-07-01 17:15:39 -0400

    Comment to explain the monkey patch

--- test/sqlite3_monkey_patch.rb
+++ test/sqlite3_monkey_patch.rb
@@ -1,3 +1,9 @@
+=begin
+This monkey patch is necessary because the SQLite3Adapter is smart enough to encode strings
+that come in with Encoding::ASCII-8BIT to Encoding::UTF-8 but others aren't.
+So to mimic the worst case, for example using a bad oracle adapter, we monkey patch the
+SQLite3Adapter to just return the string instead of trying to encode it using utf8.
+=end
 require 'active_record/connection_adapters/sqlite3_adapter'
 
 module ActiveRecord
