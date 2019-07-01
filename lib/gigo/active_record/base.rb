59a695ce8ca60bbf3696e7d96c1e5e1fb7f72541
malikhijazi <malik.hijazi@gmail.com>
2019-07-01 17:09:44 -0400

    Pass an instance of GigoCoder instead of metaprogramming

--- lib/gigo/active_record/base.rb
+++ lib/gigo/active_record/base.rb
@@ -1,83 +1,77 @@
 module GIGO
   module ActiveRecord
     module Base
-
-      module Shared
-
-        def gigo_column(*attrs)
-          mod = begin
-            if const_defined?(:GIGOColumns)
-              const_get(:GIGOColumns)
-            else
-              m = const_set(:GIGOColumns, Module.new)
-              include m
-              m
-            end
+      def gigo_column(*attrs)
+        mod = begin
+          if const_defined?(:GIGOColumns)
+            const_get(:GIGOColumns)
+          else
+            m = const_set(:GIGOColumns, Module.new)
+            include m
+            m
           end
-          attrs.each do |attr|
-            mod.module_eval <<-CODE, __FILE__, __LINE__
-              def #{attr}
-                begin
-                  GIGO.load(super)
-                rescue NoMethodError, ActiveModel::MissingAttributeError
-                  nil
-                end
+        end
+        attrs.each do |attr|
+          mod.module_eval <<-CODE, __FILE__, __LINE__
+            def #{attr}
+              begin
+                GIGO.load(super)
+              rescue NoMethodError, ActiveModel::MissingAttributeError
+                nil
               end
-            CODE
-          end
-        end
-
-        def gigo_columns(*excepts)
-          cols = columns.select{ |c| c.type == :string || c.type == :text }.map{ |c| c.name.to_s } - excepts.map(&:to_s)
-          gigo_column(*cols)
-        end
-
-        def after_initialize_gigoize_attributes
-          after_initialize :gigoize_attributes
-          define_method :gigoize_attributes do
-            self.class.const_get(:GIGOColumns).instance_methods.each { |name| self.send(name) }
-          end
-          private :gigoize_attributes
-        end
-
-        def gigo_coder_for(klass)
-          coder = Class.new
-          coder.cattr_accessor :klass
-          coder.klass = klass
-          coder.extend GigoCoder
-          return coder
-        end
-
-        module GigoCoder
-          def load(yaml)
-            return klass.new if yaml.nil?
-            existing_encoding = Encoding.default_internal
-            Encoding.default_internal = GIGO.encoding
-            value = YAML.load(GIGO.load(yaml))
-            unless value.is_a?(klass)
-              raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{hash.class}."
             end
-            value
-          ensure
-            Encoding.default_internal = existing_encoding
-          end
-
-          def dump(value)
-            return klass.new.to_yaml if value.nil?
-            unless value.is_a?(klass)
-              raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{value.class}."
-            end
-            value.to_yaml
-          end
+          CODE
         end
       end
 
-      module FiveAndUp
-        include Shared
+      def gigo_columns(*excepts)
+        cols = columns.select{ |c| c.type == :string || c.type == :text }.map{ |c| c.name.to_s } - excepts.map(&:to_s)
+        gigo_column(*cols)
+      end
+
+      def after_initialize_gigoize_attributes
+        after_initialize :gigoize_attributes
+        define_method :gigoize_attributes do
+          self.class.const_get(:GIGOColumns).instance_methods.each { |name| self.send(name) }
+        end
+        private :gigoize_attributes
+      end
+
+      def gigo_coder_for(klass)
+        GigoCoder.new(klass)
+      end
+
+      class GigoCoder
+        attr_reader :klass
+
+        def initialize(klass)
+          @klass = klass
+        end
+
+        def load(yaml)
+          return klass.new if yaml.nil?
+          existing_encoding = Encoding.default_internal
+          Encoding.default_internal = GIGO.encoding
+          value = YAML.load(GIGO.load(yaml))
+          unless value.is_a?(klass)
+            raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{hash.class}."
+          end
+          value
+        ensure
+          Encoding.default_internal = existing_encoding
+        end
+
+        def dump(value)
+          return klass.new.to_yaml if value.nil?
+          unless value.is_a?(klass)
+            raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{value.class}."
+          end
+          value.to_yaml
+        end
       end
     end
   end
 end
 
-ActiveRecord::Base.extend GIGO::ActiveRecord::Base::FiveAndUp
+ActiveRecord::Base.extend GIGO::ActiveRecord::Base
 
