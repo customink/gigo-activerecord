991a5c7aae4eb8f987488f6c09fdfd10ab68dc02
Masatoshi N <nishiguchi.masa@gmail.com>
2022-02-02 15:23:47 -0500

    Include a value in the error message for debuging ease

--- lib/gigo/active_record/base.rb
+++ lib/gigo/active_record/base.rb
@@ -57,7 +57,7 @@ module GIGO
           Encoding.default_internal = GIGO.encoding
           value = YAML.load(GIGO.load(yaml))
           unless value.is_a?(klass)
-            raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{value.class}."
+            raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{value.class}: #{value.inspect}"
           end
           value
         ensure
@@ -67,7 +67,7 @@ module GIGO
         def dump(value)
           return klass.new.to_yaml if value.nil?
           unless value.is_a?(klass)
-            raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{value.class}."
+            raise SerializationTypeMismatch, "Attribute was supposed to be a #{klass.to_s}, but was a #{value.class}: #{value.inspect}"
           end
           value.to_yaml
         end
